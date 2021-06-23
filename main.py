436f14d812cf3a24bbb7b1b2bf1a11f29c5423b1
Andrew SirLordX Serati <andrewse2015@hotmail.com>
2021-06-23 00:00:07 -0300

    configure HTTP requests for my app, specially Movie requests

--- main.py
+++ main.py
@@ -3,26 +3,64 @@ import json
 
 app = Flask(__name__)
 
+list_movies = [
+    {
+        "id": 1,
+        "title": "Star Wars: The Force Awakens",
+        "actors": ["Mark Hammil", "Harrison Ford", "Carrie Fisher"],
+        "description": "........."
+    },
+    {
+        "id": 2,
+        "title": "Avengers: Infinity War",
+        "actors": ["Robert Downey Jr.", "Scarlett Johansson", "Chris Evans"],
+        "description": "The best movie of UCM"
+    }
+]
+
+excluded_message = {
+    "message": "Movie excluded successfully!"
+}
+
 
 @app.route('/', methods=['GET'])
 def get_root_page():
-    return 'Hello World!'
+    return {"message": "Welcome to CinemaManager API. We're not the IDMb, but we're almost there ;)",
+            "status": "This API is working!"
+            }
 
 
 @app.route('/all_movies', methods=['GET'])
 def get_all_movies():
-    return jsonify({'id': 1, 'title': 'The Avengers', 'description': 'A great movie!'})
+    return jsonify(list_movies)
 
 
-@app.route('/movie/<int:id>', methods=['GET'])
+@app.route('/movie/<int:id>/', methods=['GET', 'PUT', 'DELETE'])
 def get_movie_per_id(id):
-    return jsonify({'id': id, 'title': 'The Avengers', 'description': 'A great movie!'})
+
+    if request.method == 'GET':
+        try:
+            response = list_movies[id-1]
+        except IndexError:
+            response = {"message": "There are no movie with id {}, sorry :/".format(id)}
+        except Exception:
+            response = {"message": "Unknown Error. Sooo bad...."}
+        return jsonify(response)
+
+    elif request.method == 'PUT':
+        datas = json.loads(request.data)
+        list_movies[id] = datas
+        return jsonify(datas)
+    elif request.method == 'DELETE':
+        list_movies.pop(id)
+        return jsonify(excluded_message)
 
 
 @app.route('/add_movie', methods=['POST'])
 def post_movie():
-    datas = request.data
-    return datas
+    datas = json.loads(request.data)
+    list_movies.append(datas)
+    return {"message": "Movie created successfully"}
 
 
 if __name__ == "__main__":
